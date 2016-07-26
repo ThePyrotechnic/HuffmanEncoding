c2ff4b0fbe6158a9172c871de723cc76c79a4eb6
ThePyrotechnic <electricpyrotechnic@gmail.com>
2016-07-25 23:30:50 -0400

    Added decoding

--- Trees/src/huffmanEncoding/HuffmanTest.java
+++ Trees/src/huffmanEncoding/HuffmanTest.java
@@ -49,17 +49,19 @@ public class HuffmanTest {
 				sort(nodeList);
 			}
 		}
-		
+
 		ArrayList<HuffCodePair> codePairs = traverse(nodeList.get(0));
-		
+
 		String codedInput = "";
-		for(Character cur : input.toCharArray())
+		for (Character cur : input.toCharArray())
 			codedInput += getCode(cur, codePairs);
-		float compressionAmt = 100 - ((float) codedInput.length()/((float) (input.getBytes().length*8)) * 100);
-	
-		System.out.println("Output: " + codedInput + "\nBits: " + codedInput.length() + 
-				"\nCompression amount: "+compressionAmt+"%");
-		
+		float compressionAmt = 100 - ((float) codedInput.length() / ((float) (input.getBytes().length * 8)) * 100);
+
+		System.out.println("Output: " + codedInput + "\nBits: " + codedInput.length() + "\nCompression amount: "
+				+ compressionAmt + "%");
+
+		System.out.println("Decoded binary: "+decode(codedInput, codePairs));
+
 		scan.close();
 	}
 
@@ -98,12 +100,46 @@ public class HuffmanTest {
 		if (node.getRight() != null)
 			traverse(node.getRight(), code + "0", codePairs);
 	}
-	
+
 	private static String getCode(Character cur, ArrayList<HuffCodePair> codeList) {
-		for(int a = 0;a < codeList.size(); a++){
-			if(cur.toString().equals(codeList.get(a).getID())) return codeList.get(a).getCode();
+		for (int a = 0; a < codeList.size(); a++) {
+			if (cur.toString().equals(codeList.get(a).getID()))
+				return codeList.get(a).getCode();
 		}
 		return "";
 	}
-	
+
+	private static String decode(String input, ArrayList<HuffCodePair> codeList) {
+		String result = "";
+		char[] inputArr = input.toCharArray();
+
+
+		for (int b = 0; b < input.length();) {
+			boolean found = false;
+			String cur = "" + inputArr[b];
+			while (!found) {
+				if (linearSearch(cur, codeList)) {
+					result += extract(cur,codeList);
+					found = true;
+					b++;
+				} else
+					cur += inputArr[++b];
+			}
+		}
+		return result;
+	}
+
+	private static boolean linearSearch(String query, ArrayList<HuffCodePair> codeList) {
+		for (int a = 0; a < codeList.size(); a++)
+			if (codeList.get(a).getCode().equals(query))
+				return true;
+		return false;
+	}
+	private static String extract(String cur, ArrayList<HuffCodePair> codeList) {
+		String result = "";
+		for (int a = 0; a < codeList.size(); a++)
+			if (codeList.get(a).getCode().equals(cur))
+				result = codeList.get(a).getID();
+		return result;
+	}
 }
